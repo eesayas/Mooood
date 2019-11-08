36e67ec2d28df6ec8ad39b1615c7ca46a28693f9
Max Melendez <mmelende@ualberta.ca>
2019-11-07 23:26:54 -0700

    Unit Testing for add mood

---
+++ app/src/androidTest/java/com/example/mooood/AddMoodTest.java
@@ -0,0 +1,63 @@
+package com.example.mooood;
+import android.app.Activity;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.fragment.app.DialogFragment;
+
+import com.robotium.solo.Solo;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+@RunWith(AndroidJUnit4.class)
+public class AddMoodTest {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance.
+     *
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     *
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+    @Test
+    public void checkAddDefault(){
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.enterText((EditText)solo.getView(R.id.activity_main_et__username), "hyeon");
+        solo.waitForText("hyeon",1,2000);
+        solo.enterText((EditText)solo.getView(R.id.activity_main_et__password), "1");
+        solo.waitForText("1",1,2000);
+        solo.clickOnView(solo.getView(R.id.activity_main_btn_submit));
+        solo.waitForActivity(UserFeedActivity.class);
+        solo.clickOnView(solo.getView(R.id.fab));
+        solo.waitForActivity(CreateEventActivity.class);
+        //solo.clickOnView(solo.getView(R.id.));
+    }
+
+}
