dfea7b210d8dd9f0f7a8073461cb06622eb35658
Max Melendez <mmelende@ualberta.ca>
2019-11-08 00:48:09 -0700

    Added Add Mood testing

--- app/src/androidTest/java/com/example/mooood/AddMoodTest.java
+++ app/src/androidTest/java/com/example/mooood/AddMoodTest.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import androidx.fragment.app.DialogFragment;
 
 import com.robotium.solo.Solo;
@@ -22,6 +24,7 @@ import static org.junit.Assert.assertFalse;
 @RunWith(AndroidJUnit4.class)
 public class AddMoodTest {
     private Solo solo;
+    public static Calendar calendar;
 
     @Rule
     public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
@@ -47,17 +50,56 @@ public class AddMoodTest {
     }
 
     @Test
-    public void checkAddDefault(){
+    public void checkAddMood(){
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.enterText((EditText)solo.getView(R.id.activity_main_et__username), "hyeon");
-        solo.waitForText("hyeon",1,2000);
-        solo.enterText((EditText)solo.getView(R.id.activity_main_et__password), "1");
-        solo.waitForText("1",1,2000);
+        solo.enterText((EditText)solo.getView(R.id.activity_main_et__username), "coolerman");
+        solo.waitForText("coolerman",1,2000);
+        solo.enterText((EditText)solo.getView(R.id.activity_main_et__password), "coolerman");
+        solo.waitForText("coolerman",1,2000);
         solo.clickOnView(solo.getView(R.id.activity_main_btn_submit));
         solo.waitForActivity(UserFeedActivity.class);
+
+        //Create mood event with: happy (default emoji), this is a reason, alone, current date and time, no picture for now, no location for now.
         solo.clickOnView(solo.getView(R.id.fab));
         solo.waitForActivity(CreateEventActivity.class);
-        //solo.clickOnView(solo.getView(R.id.));
+        solo.enterText((EditText) solo.getView(R.id.reason),"This is a reason");
+        solo.clickOnView(solo.getView(R.id.social_situation));
+        solo.clickOnText("Alone");
+        solo.clickOnView(solo.getView(R.id.date_and_time));
+        solo.waitForText("OK",1,2000);
+        solo.clickOnText("OK");
+        solo.waitForText("OK",1,2000);
+        solo.clickOnText("OK");
+        calendar = Calendar.getInstance();
+        solo.clickOnView(solo.getView(R.id.submit_button));
+        solo.waitForActivity(UserFeedActivity.class);
+    }
+
+    @Test
+    public void checkMoodInList(){
+        //Formats the time correctly
+        SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm:ss a");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd yyyy");
+        String selectedTime = timeFormat.format(calendar.getTime());
+        String selectedDate = dateFormat.format(calendar.getTime());
+
+        //Log into user feed
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.enterText((EditText)solo.getView(R.id.activity_main_et__username), "coolerman");
+        solo.waitForText("coolerman",1,200);
+        solo.enterText((EditText)solo.getView(R.id.activity_main_et__password), "coolerman");
+        solo.waitForText("coolerman",1,200);
+        solo.clickOnView(solo.getView(R.id.activity_main_btn_submit));
+        solo.waitForActivity(UserFeedActivity.class);
+
+        //Check that mood event exists, i.e, checking persistency.
+        solo.waitForText("coolerman",1,200);
+        solo.clickOnText("coolerman");
+        solo.waitForText("coolerman",1,200);
+        solo.waitForText(selectedDate,1,200);
+        solo.waitForText(selectedTime,1,200);
+        solo.waitForText("Alone",1,200);
+        solo.waitForText("This is a reason",1,200);
     }
 
 }
