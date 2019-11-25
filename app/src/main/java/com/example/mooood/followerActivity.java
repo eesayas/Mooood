bc63d11714239b40949d91efc233cff1e151b207
Hyeon Yu <hyeon@ualberta.ca>
2019-11-24 17:57:43 -0700

    xml for followerAct

--- app/src/main/java/com/example/mooood/followerActivity.java
+++ app/src/main/java/com/example/mooood/followerActivity.java
@@ -1,10 +1,14 @@
 package com.example.mooood;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Show profile of searched user and can send request
+ */
 public class followerActivity extends AppCompatActivity {
 
     @Override
@@ -14,5 +18,22 @@ public class followerActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         final MoodEvent moodEvent = intent.getParcelableExtra("accountMood");
+
+        // Set up display
+    }
+
+
+    public void requestedOrCancel(View view) {
+        // change button into requested if clicked again cancel the request
+
+    }
+
+
+    public void backToSearch(View view) {
+//        figure out a way to go back with filled listview
+//        Intent intent = new Intent(followerActivity.this, feedActivity.class );
+//        startActivity(intent);
+        finish();
+//        ?????????
     }
 }
