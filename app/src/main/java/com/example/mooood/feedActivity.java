7b37bf2732b147fcce30ac9dca5f9bc0e3fec79a
Maaz Siddique <maazsidd@ualberta.ca>
2019-11-27 09:13:59 -0700

    Fixed profile activity, can now click on it from both User and Feed activity and will work without crashing

--- app/src/main/java/com/example/mooood/feedActivity.java
+++ app/src/main/java/com/example/mooood/feedActivity.java
@@ -56,13 +56,13 @@ public class feedActivity extends AppCompatActivity {
     Date moodTimeStamp;
     String edit;
     private String name;
+    TextView userProfile;
 
 
     //Firebase setup
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference collectionReference;
     private CollectionReference feedCollectionReference;
-
 
     private TextView userId;
 
@@ -91,6 +91,7 @@ public class feedActivity extends AppCompatActivity {
         selectUser();
         notificationCheck(name);
         showEventClickListener();
+        goToProfile();
         feedDataList.clear();
         Adapter.notifyDataSetChanged();
 
@@ -345,44 +346,54 @@ public class feedActivity extends AppCompatActivity {
         });
     }
 
-   /* *//**
+    /**
      * This is a click listener to go to profile
-     *//*
-    public void goToProfile(View view) {
+     */
+    public void goToProfile(){
+        userProfile= findViewById(R.id.activity_feed_show_profile);
+        userProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                collectionReference.document(name).collection("MoodActivities")
+                        .orderBy("timeStamp", Query.Direction.DESCENDING)
+                        .limit(1)
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                feedDataList.clear();
+                                for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
+                                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd yyyy h:mm a");
 
-        db.collection("Users")
-                .whereEqualTo("author", name)
-                .limit(1)
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        feedDataList.clear();
-                        for (QueryDocumentSnapshot documentSnapshot : task.getResult()) {
-                            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM dd yyyy h:mm a");
-
-                            String author = (String) documentSnapshot.getData().get("author");
-                            String date = (String) documentSnapshot.getData().get("date");
-                            String time = (String) documentSnapshot.getData().get("time");
-                            String emotionalState = (String) documentSnapshot.getData().get("emotionalState");
-                            String reason = (String) documentSnapshot.getData().get("reason");
-                            String socialSituation = (String) documentSnapshot.getData().get("socialSituation");
+                                    String author = (String) documentSnapshot.getData().get("author");
+                                    String date = (String) documentSnapshot.getData().get("date");
+                                    String time = (String) documentSnapshot.getData().get("time");
+                                    String emotionalState = (String) documentSnapshot.getData().get("emotionalState");
+                                    String reason = (String) documentSnapshot.getData().get("reason");
+                                    String socialSituation = (String) documentSnapshot.getData().get("socialSituation");
 
 
-                            Intent intent = new Intent(feedActivity.this, UserProfile.class);
+                                    Intent intent = new Intent(feedActivity.this, UserProfile.class);
 
-                            intent.putExtra("AUTHOR", author);
-                            intent.putExtra("DATE", date);
-                            intent.putExtra("TIME", time);
-                            intent.putExtra("STATE", emotionalState);
-                            intent.putExtra("REASON", reason);
-                            intent.putExtra("SITUATION", socialSituation);
-                            startActivity(intent);
-                        }
+                                    intent.putExtra("AUTHOR", author);
+                                    intent.putExtra("DATE", date);
+                                    intent.putExtra("TIME", time);
+                                    intent.putExtra("STATE", emotionalState);
+                                    intent.putExtra("REASON", reason);
+                                    intent.putExtra("SITUATION", socialSituation);
+                                    startActivity(intent);
+                                }
 
-                    }
-                });
-    }*/
+                            }
+                        });
+            }
+
+        });
+
+    }
+
+
+
 
 
 
